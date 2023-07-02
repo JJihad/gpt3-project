b8d6ffdb4237f9117cce06c444d2c4793b6c6ec2
JJihad <jbr.jihad@gmail.com>
2023-07-02 16:42:34 -0400

    brands completed
    list of brands partners

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './app.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='gradient__bg'>
+    <div className="App">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
+        <Brand />
       </div>
 
-      <Brand />
       <WhatGPT3 />
       <Features />
       <Possibility />
       <Blog />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App
